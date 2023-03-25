eb9efbd24cb01f9847d22d68e6e09dd5acd71b8c
Vikash Sharma <kvikashsharma09@gmail.com>
2023-03-25 13:02:53 +0530

    forwardRef is used

--- usereducer/src/components/Login/Login.js
+++ usereducer/src/components/Login/Login.js
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useReducer, useContext } from "react";
+import React, {
+  useState,
+  useEffect,
+  useReducer,
+  useContext,
+  useRef,
+} from "react";
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
@@ -40,6 +46,10 @@ const Login = () => {
   });
 
   const ctxAuth = useContext(AuthContext);
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
   useEffect(() => {
     console.log("EFFECT RUNNING");
     return () => {
@@ -84,13 +94,20 @@ const Login = () => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    ctxAuth.onLogin(emailState.value, passwordState.value);
+    if (formIsValid) {
+      ctxAuth.onLogin(emailState.value, passwordState.value);
+    } else if (!emailIsValid) {
+      emailInputRef.current.focus();
+    } else {
+      passwordInputRef.current.focus();
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+          ref={emailInputRef}
           id="email"
           label="E-Mail"
           type="email"
@@ -100,6 +117,7 @@ const Login = () => {
           onBlur={validateEmailHandler}
         />
         <Input
+          ref={passwordInputRef}
           id="password"
           label="Password"
           type="password"
@@ -109,11 +127,7 @@ const Login = () => {
           onBlur={validatePasswordHandler}
         />
         <div className={classes.actions}>
-          <Button
-            type="submit"
-            className={classes.button}
-            disabled={!formIsValid}
-          >
+          <Button type="submit" className={classes.button}>
             Login
           </Button>
         </div>
--- usereducer/src/components/UI/Input/Input.js
+++ usereducer/src/components/UI/Input/Input.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useRef, useImperativeHandle } from "react";
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  const activate = () => {
+    inputRef.current.focus();
+  };
+  useImperativeHandle(ref, () => {
+    return {
+      focus: activate,
+    };
+  });
+  // useEffect(() => {
+  //   inputRef.current.focus();
+  // }, []);
   return (
     <div
       className={`${classes.control} ${
@@ -10,6 +23,7 @@ const Input = (props) => {
     >
       <label htmlFor={props.id}>{props.label}</label>
       <input
+        ref={inputRef}
         type={props.type}
         id={props.id}
         value={props.value}
@@ -18,5 +32,5 @@ const Input = (props) => {
       />
     </div>
   );
-};
+});
 export default Input;
